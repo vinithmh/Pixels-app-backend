aa7de5318f5881dec8beb0feece1105b8b816adf
Vinith M H <vinithmh2000@gmail.com>
2023-01-20 02:28:44 +0530

    authentication

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import dotenv from "dotenv";
 const PORT = process.env.PORT || 5000;
 
 import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/posts.js";
 
 const app = express();
 dotenv.config();
@@ -15,6 +16,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
+app.use("/user", userRoutes);
 
 const CONNECTION_URL = process.env.CONNECTION_URL;
 
---
+++ models/user.js
@@ -0,0 +1,21 @@
+import mongoose, { MongooseError, trusted } from "mongoose";
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  id: {
+    type: String,
+  },
+});
+
+export default mongoose.model("User", userSchema);
