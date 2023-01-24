c89994f3ea472bb2e48cc35e7e388a8528a6af82
Vinith M H <vinithmh2000@gmail.com>
2023-01-24 20:35:48 +0530

    Pagination

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 import {
+  getPostsBySearch,
   getPosts,
   createPost,
   updatePost,
@@ -10,6 +11,8 @@ import {
 import auth from "../middleware/auth.js";
 
 const router = express.Router();
+
+router.get("/search", getPostsBySearch);
 
 router.get("/", getPosts);
 
